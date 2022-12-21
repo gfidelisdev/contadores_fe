59b8d982785e4535f6a9f0aeac3bd5f960fea05f
Guilherme <guilherme.silva@mpt.mp.br>
2022-12-21 10:56:16 -0300

    first

---
+++ src/components/ByMonth.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-container fluid>
+    <v-row>
+        <v-col>
+            <!-- Selectors for month and year, and a submit button -->
+            <v-select
+                :items="items"
+                v-model="month"
+                label="Mês"
+            ></v-select>
+
+        </v-col>
+        <v-col>
+            <v-btn icon color="success">
+                <v-icon color="white">mdi-check</v-icon>
+            </v-btn>
+
+        </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            month: 'Janeiro',
+            items:[
+                'Janeiro',
+                'Fevereiro',
+                'Março',
+                'Abril',
+                'Maio',
+                'Junho',
+                'Julho',
+                'Agosto',
+                'Setembro',
+                'Outubro',
+                'Novembro',
+                'Dezembro'
+            ]
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-container fluid grid-list-xs>
+        <!-- Top Tabs-->
+        <topnav />
+        <by-month />
+        <!-- Main body -->
+    </v-container>
+</template>
+
+<script>
+import ByMonth from './ByMonth.vue'
+import Topnav from './Topnav.vue'
+export default {
+  components: { Topnav, ByMonth },
+
+}
+</script>
+
+<style>
+
+</style>
